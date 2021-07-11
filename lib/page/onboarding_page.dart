47cd58506eaa4b5024807504baa81af7c0b3f57d
Mwita04 <emcee779@gmail.com>
2021-07-11 23:16:10 +0300

    first commit

---
+++ lib/page/onboarding_page.dart
@@ -0,0 +1,9 @@
+import 'package:material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => SafeArea(
+        child: IntroductionScreen(),
+      );
+}
