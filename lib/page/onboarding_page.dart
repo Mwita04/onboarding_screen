d7081aaeb5abb9beae89b370880af422c947a4f5
Mwita04 <emcee779@gmail.com>
2021-07-12 13:16:49 +0300

    commit on master

--- lib/page/onboarding_page.dart
+++ lib/page/onboarding_page.dart
@@ -1,8 +1,7 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:onboarding_screen/page/home_page.dart';
 
 // ignore: use_key_in_widget_constructors
 class OnBoardingPage extends StatelessWidget {
@@ -14,12 +13,44 @@ class OnBoardingPage extends StatelessWidget {
               title: 'A reader lives a thousand lives',
               body: 'The man who never reads lives only once.',
               image: buildImage('assets/ebook.png'),
+              decoration: getPageDecoration(),
+            ),
+            PageViewModel(
+              title: 'Featured Books',
+              body: 'Available right at your fingertips.',
+              image: buildImage('assets/readingbook.png'),
+              decoration: getPageDecoration(),
+            ),
+            PageViewModel(
+              title: 'Simple UI',
+              body: 'For enhanced rading experience',
+              image: buildImage('assets/manthumbs.png'),
+              decoration: getPageDecoration(),
+            ),
+            PageViewModel(
+              title: 'Today reader, tomorrow a leader',
+              body: 'Start your journey',
+              image: buildImage('assets/learn.png'),
+              decoration: getPageDecoration(),
             ),
           ],
           done: Text('Read', style: TextStyle(fontWeight: FontWeight.w600)),
-          onDone: () {},
+          onDone: () => goToHome(context),
         ),
       );
+
+  void goToHome(context) => Navigator.of(context).pushReplacement(
+    MaterialPageRoute(builder: (_) => Homepage()),
+  );
+
   Widget buildImage(String path) =>
       Center(child: Image.asset(path, width: 350));
+
+  PageDecoration getPageDecoration() => PageDecoration(
+        titleTextStyle: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
+        bodyTextStyle: TextStyle(fontSize: 20),
+        descriptionPadding: EdgeInsets.all(16).copyWith(bottom: 0),
+        imagePadding: EdgeInsets.all(24),
+        pageColor: Colors.orange,
+      );
 }
