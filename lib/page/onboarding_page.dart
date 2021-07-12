0a7d8bcd6fedcea31fb027207401170b2ae78fa6
Mwita04 <emcee779@gmail.com>
2021-07-12 12:34:32 +0300

    commit on master

--- lib/page/onboarding_page.dart
+++ lib/page/onboarding_page.dart
@@ -1,9 +1,25 @@
-import 'package:material.dart';
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 
+// ignore: use_key_in_widget_constructors
 class OnBoardingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => SafeArea(
-        child: IntroductionScreen(),
+        child: IntroductionScreen(
+          pages: [
+            PageViewModel(
+              title: 'A reader lives a thousand lives',
+              body: 'The man who never reads lives only once.',
+              image: buildImage('assets/ebook.png'),
+            ),
+          ],
+          done: Text('Read', style: TextStyle(fontWeight: FontWeight.w600)),
+          onDone: () {},
+        ),
       );
+  Widget buildImage(String path) =>
+      Center(child: Image.asset(path, width: 350));
 }
